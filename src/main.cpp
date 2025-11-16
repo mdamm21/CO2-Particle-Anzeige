77f482d235893cfb2bc2b0c056efe43f867f6e9d
Max Damm <Max.Damm@Student.HTW-Berlin.de>
2025-11-16 16:08:58 +0100

    Sensor an A0 angeschlossen, gibt jtz alle 100ms den aktuellen Sensorwert an den serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+int sensorWert = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode (A0, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  sensorWert = analogRead(A0);
+  Serial.println(sensorWert); // dann hier display code
+  delay(100);
 }
