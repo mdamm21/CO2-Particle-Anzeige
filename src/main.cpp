4cd9923ba216605f89a832494b65e1c8d923efac
Max Damm <Max.Damm@Student.HTW-Berlin.de>
2025-11-18 18:22:01 +0100

    Sensor trackt erfolgreich die ppms in den Serial Monitor

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,13 @@
 int sensorWert = 0;
 
 void setup() {
+  Serial.begin(9600);
   pinMode (A0, INPUT);
   // digital pins für LCD Display
 }
 
 void loop() {
   sensorWert = analogRead(A0);
-  Serial.println(sensorWert); // gerade noch wichtig um experimentell den eigentlichen grenzwert zu finden 
-  delay(100);
-  if (sensorWert >= 150){ // 150 ist erstmal willkürlich
-    // display sag "Lüften"
-  } else {
-    // display chill 
-  }
+  Serial.println(sensorWert); // Grenzwert evtl 105ppm 
+  delay(1000);
 }
